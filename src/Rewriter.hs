5db15fd1584f5361c25561f8bd9de3768d310d5e
maxonfjvipon <mtrunnikov@gmail.com>
2025-05-07 21:38:47 +0300

    rewriter with maybe

--- src/Rewriter.hs
+++ src/Rewriter.hs
@@ -1,53 +1,62 @@
 -- SPDX-FileCopyrightText: Copyright (c) 2025 Objectionary.com
 -- SPDX-License-Identifier: MIT
 
-module Rewriter where
+-- The goal of the module is to rewrite Program with replacing
+-- pattern sub expression with target expressions
+module Rewriter (rewriteProgram) where
 
 import Ast
 import Matcher (Tail (TaApplication, TaDispatch))
 
-rewriteTauBinding :: TauBinding -> Expression -> [Expression] -> (TauBinding, [Expression])
-rewriteTauBinding (TauBinding attr expr) ptn repls = do
-  let (expr', rest) = rewriteExpression expr ptn repls
-  (TauBinding attr expr', rest)
+rewriteTauBinding :: TauBinding -> [Expression] -> [Expression] -> (TauBinding, [Expression], [Expression])
+rewriteTauBinding (TauBinding attr expr) ptns repls = do
+  let (expr', ptns', repls') = rewriteExpression expr ptns repls
+  (TauBinding attr expr', ptns', repls')
 
-rewriteBindings :: [Binding] -> Expression -> [Expression] -> ([Binding], [Expression])
-rewriteBindings bds _ [] = (bds, [])
-rewriteBindings [] _ repls = ([], repls)
-rewriteBindings (BiTau tau:bds) ptn repls = do
-  let (tau', rest) = rewriteTauBinding tau ptn repls
-  let (bds', rest') = rewriteBindings bds ptn rest
-  (BiTau tau':bds', rest')
-rewriteBindings bds _ repls = (bds, repls)
-  
-rewriteTauBindings :: [TauBinding] -> Expression -> [Expression] -> ([TauBinding], [Expression])
-rewriteTauBindings taus _ [] = (taus, [])
-rewriteTauBindings [] _ repls = ([], repls)
-rewriteTauBindings (tau:taus) ptn repls = do
-  let (tau', rest) = rewriteTauBinding tau ptn repls
-  let (taus', rest') = rewriteTauBindings taus ptn rest
-  (tau':taus', rest')
+rewriteTauBindings :: [TauBinding] -> [Expression] -> [Expression] -> ([TauBinding], [Expression], [Expression])
+rewriteTauBindings taus [] [] = (taus, [], [])
+rewriteTauBindings [] ptns repls = ([], ptns, repls)
+rewriteTauBindings (tau : taus) ptns repls = do
+  let (tau', ptns', repls') = rewriteTauBinding tau ptns repls
+  let (taus', ptns'', repls'') = rewriteTauBindings taus ptns' repls'
+  (tau' : taus', ptns'', repls'')
 
-rewriteExpression :: Expression -> Expression -> [Expression] -> (Expression, [Expression])
-rewriteExpression expr ptn [] = (expr, [])
-rewriteExpression expr ptn repls = do
-  let (repl:repls') = repls
+rewriteBindings :: [Binding] -> [Expression] -> [Expression] -> ([Binding], [Expression], [Expression])
+rewriteBindings bds [] [] = (bds, [], [])
+rewriteBindings [] ptns repls = ([], ptns, repls)
+rewriteBindings (BiTau tau : bds) ptns repls = do
+  let (tau', ptns', repls') = rewriteTauBinding tau ptns repls
+  let (bds', ptns'', repls'') = rewriteBindings bds ptns' repls'
+  (BiTau tau' : bds', ptns'', repls'')
+rewriteBindings (bd : bds) ptns repls = do
+  let (bds', ptns', repls') = rewriteBindings bds ptns repls
+  (bd : bds', ptns', repls')
+
+rewriteExpression :: Expression -> [Expression] -> [Expression] -> (Expression, [Expression], [Expression])
+rewriteExpression expr [] [] = (expr, [], [])
+rewriteExpression expr ptns repls = do
+  let (ptn : ptnsRest) = ptns
+  let (repl : replsRest) = repls
   if expr == ptn
-    then (repl, repls')
+    then (repl, ptnsRest, replsRest)
     else case expr of
       ExDispatch inner attr -> do
-        let (expr', rest) = rewriteExpression inner ptn repls
-        (ExDispatch expr' attr, rest)
-      ExFormation bds -> do
-        let (bds', rest) = rewriteBindings bds ptn repls
-        (ExFormation bds', rest)
+        let (expr', ptns', repls') = rewriteExpression inner ptns repls
+        (ExDispatch expr' attr, ptns', repls')
       ExApplication inner taus -> do
-        let (expr', rest) = rewriteExpression inner ptn repls
-        let (taus', rest') = rewriteTauBindings taus ptn rest
-        (ExApplication expr' taus', rest')
-      _ -> (expr, repls)
+        let (expr', ptns', repls') = rewriteExpression inner ptns repls
+        let (taus', ptns'', repls'') = rewriteTauBindings taus ptns' repls'
+        (ExApplication expr' taus', ptns'', repls'')
+      ExFormation bds -> do
+        let (bds', ptns', repls') = rewriteBindings bds ptns repls
+        (ExFormation bds', ptns', repls')
+      _ -> (expr, ptns, repls)
 
-rewriteProgram :: Program -> Expression -> [Expression] -> Program
-rewriteProgram (Program expr) ptn repls = do
-  let (expr', _) = rewriteExpression expr ptn repls
-  Program expr'
+-- >>> rewriteProgram (Program (ExDispatch ExThis (AtLabel "x"))) [ExThis] [ExGlobal]
+-- Program (ExDispatch ExGlobal (AtLabel "x"))
+rewriteProgram :: Program -> [Expression] -> [Expression] -> Maybe Program
+rewriteProgram (Program expr) ptns repls
+  | length ptns == length repls = do
+      let (expr', _, _) = rewriteExpression expr ptns repls
+      Just (Program expr')
+  | otherwise = Nothing
--- test/RewriterSpec.hs
+++ test/RewriterSpec.hs
@@ -4,17 +4,46 @@
 module RewriterSpec where
 
 import Ast
+import Control.Monad (forM_)
 import Rewriter
-import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Hspec (Example (Arg), Expectation, Spec, SpecWith, describe, it, shouldBe)
+
+test ::
+  (Program -> [Expression] -> [Expression] -> Maybe Program) ->
+  [(String, Program, [Expression], [Expression], Maybe Program)] ->
+  SpecWith (Arg Expectation)
+test function useCases =
+  forM_ useCases $ \(desc, prog, ptns, repls, res) ->
+    it desc $ function prog ptns repls `shouldBe` res
 
 spec :: Spec
 spec = do
   describe "rewriteProgram" $ do
-    it "rewrites" $ do
-      shouldBe
-        ( rewriteProgram
-            (Program (ExApplication (ExDispatch (ExDispatch ExGlobal (AtLabel "y")) (AtLabel "x")) []))
-            (ExDispatch ExGlobal (AtLabel "y"))
-            [ExThis]
-        )
-        (Program (ExApplication (ExDispatch ExThis (AtLabel "x")) []))
+    test
+      rewriteProgram
+      [ ( "Q -> Q.y.x() => ([Q.y], [$]) => Q -> $.x()",
+          Program (ExApplication (ExDispatch (ExDispatch ExGlobal (AtLabel "y")) (AtLabel "x")) []),
+          [ExDispatch ExGlobal (AtLabel "y")],
+          [ExThis],
+          Just (Program (ExApplication (ExDispatch ExThis (AtLabel "x")) []))
+        ),
+        ( "Q -> [x -> [y -> $], z -> [w -> $]] => ([[y -> $], [w -> $]], [Q.y, Q.w]) => Q -> [x -> Q.y, z -> Q.w]",
+          Program
+            ( ExFormation
+                [ BiTau (TauBinding (AtLabel "x") (ExFormation [BiTau (TauBinding (AtLabel "y") ExThis)])),
+                  BiTau (TauBinding (AtLabel "z") (ExFormation [BiTau (TauBinding (AtLabel "w") ExThis)]))
+                ]
+            ),
+          [ExFormation [BiTau (TauBinding (AtLabel "y") ExThis)], ExFormation [BiTau (TauBinding (AtLabel "w") ExThis)]],
+          [ExDispatch ExGlobal (AtLabel "y"), ExDispatch ExGlobal (AtLabel "w")],
+          Just
+            ( Program
+                ( ExFormation
+                    [ BiTau (TauBinding (AtLabel "x") (ExDispatch ExGlobal (AtLabel "y"))),
+                      BiTau (TauBinding (AtLabel "z") (ExDispatch ExGlobal (AtLabel "w")))
+                    ]
+                )
+            )
+        ),
+        ("Q -> [] => ([], [$]) => X", Program $ ExFormation [], [], [ExThis], Nothing)
+      ]
