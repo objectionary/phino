661449fae5fe5394e3bb42ce658097ac1d30992a
maxonfjvipon <mtrunnikov@gmail.com>
2025-05-13 15:23:16 +0300

    spdx

--- src/Printer.hs
+++ src/Printer.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 
+-- SPDX-FileCopyrightText: Copyright (c) 2025 Objectionary.com
+-- SPDX-License-Identifier: MIT
+
 module Printer (printExpression, printProgram) where
 
 import Ast
