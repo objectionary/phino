26a0cf2b2e4f0f01d38a78f39460e50387a51121
maxonfjvipon <mtrunnikov@gmail.com>
2025-06-09 13:07:52 +0300

    bug(#77): condition packs

--- test/YamlSpec.hs
+++ test/YamlSpec.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 -- SPDX-FileCopyrightText: Copyright (c) 2025 Objectionary.com
 -- SPDX-License-Identifier: MIT
 
@@ -6,8 +9,25 @@ module YamlSpec where
 import Control.Monad
 import Misc
 import System.FilePath
-import Test.Hspec (Spec, describe, it, runIO, shouldReturn)
-import Yaml (yamlRule)
+import Test.Hspec (Spec, describe, it, runIO, shouldReturn, shouldNotBe, shouldBe)
+import Yaml (yamlRule, Condition)
+import Ast (Expression, Program (Program))
+import qualified Data.Yaml as Y
+import Matcher (matchProgram)
+import Rewriter (meets)
+import Data.Aeson
+import GHC.Generics
+
+data ConditionPack = ConditionPack
+  {
+    expression :: Expression,
+    pattern :: Expression,
+    condition :: Condition
+  }
+  deriving (Generic, FromJSON, Show)
+
+conditionPack :: FilePath -> IO ConditionPack
+conditionPack = Y.decodeFileThrow
 
 spec :: Spec
 spec = do
@@ -20,3 +40,16 @@ spec = do
           _ <- yamlRule pth
           pure () `shouldReturn` ()
       )
+  
+  describe "check conditions" $ do
+    let resources = "test-resources/condition-packs"
+    packs <- runIO (allPathsIn resources)
+    forM_
+      packs
+      ( \pth -> it (makeRelative resources pth) $ do
+          pack <- conditionPack pth
+          let matched = matchProgram (pattern pack) (Program (expression pack))
+          matched `shouldNotBe` []
+          meets (condition pack) matched `shouldBe` matched
+      )
+
