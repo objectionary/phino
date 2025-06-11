635c3e86f0c607e706f4564d6d2969f2185e5220
maxonfjvipon <mtrunnikov@gmail.com>
2025-06-11 18:00:31 +0300

    bug(#89): condition spec

---
+++ test/ConditionSpec.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+-- SPDX-FileCopyrightText: Copyright (c) 2025 Objectionary.com
+-- SPDX-License-Identifier: MIT
+
+module ConditionSpec where
+
+import Ast (Expression, Program (Program))
+import Control.Monad
+import Data.Aeson
+import Data.Yaml qualified as Y
+import GHC.Generics
+import Matcher (matchProgram)
+import Misc
+import Printer (printSubstitutions)
+import Condition (meetCondition)
+import System.FilePath
+import Test.Hspec (Spec, describe, expectationFailure, it, runIO)
+import qualified Yaml as Y
+
+data ConditionPack = ConditionPack
+  { expression :: Expression,
+    pattern :: Expression,
+    condition :: Y.Condition
+  }
+  deriving (Generic, FromJSON, Show)
+
+spec :: Spec
+spec = do
+  describe "check conditions" $ do
+    let resources = "test-resources/condition-packs"
+    packs <- runIO (allPathsIn resources)
+    forM_
+      packs
+      ( \pth -> it (makeRelative resources pth) $ do
+          pack <- Y.decodeFileThrow pth :: IO ConditionPack
+          let matched = matchProgram (pattern pack) (Program (expression pack))
+          unless (matched /= []) (expectationFailure "List of matched substitutions is empty which is not expected")
+          let met = meetCondition (condition pack) matched
+          unless
+            (met == matched)
+            ( expectationFailure $
+                "Condition must not decrease the list of substitutions\nExpected:\n"
+                  ++ printSubstitutions matched
+                  ++ "\nGot:\n"
+                  ++ printSubstitutions met
+            )
+      )
--- test/YamlSpec.hs
+++ test/YamlSpec.hs
@@ -6,17 +6,13 @@
 
 module YamlSpec where
 
-import Ast (Expression, Program (Program))
+import Ast (Expression)
 import Control.Monad
 import Data.Aeson
-import Data.Yaml qualified as Y
 import GHC.Generics
-import Matcher (matchProgram)
 import Misc
-import Printer (printSubstitutions)
-import Condition (meetCondition)
 import System.FilePath
-import Test.Hspec (Spec, describe, expectationFailure, it, runIO, shouldReturn)
+import Test.Hspec (Spec, describe, it, runIO, shouldReturn)
 import Yaml (yamlRule)
 import qualified Yaml as Y
 
@@ -37,24 +33,4 @@ spec = do
       ( \pth -> it (makeRelative resources pth) $ do
           _ <- yamlRule pth
           pure () `shouldReturn` ()
-      )
-
-  describe "check conditions" $ do
-    let resources = "test-resources/condition-packs"
-    packs <- runIO (allPathsIn resources)
-    forM_
-      packs
-      ( \pth -> it (makeRelative resources pth) $ do
-          pack <- Y.decodeFileThrow pth :: IO ConditionPack
-          let matched = matchProgram (pattern pack) (Program (expression pack))
-          unless (matched /= []) (expectationFailure "List of matched substitutions is empty which is not expected")
-          let met = meetCondition (condition pack) matched
-          unless
-            (met == matched)
-            ( expectationFailure $
-                "Condition must not decrease the list of substitutions\nExpected:\n"
-                  ++ printSubstitutions matched
-                  ++ "\nGot:\n"
-                  ++ printSubstitutions met
-            )
       )
