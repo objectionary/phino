fe755fd675fe37d083164ef576e8ef48cb4bd9b1
maxonfjvipon <mtrunnikov@gmail.com>
2025-07-04 12:34:44 +0300

    bug(#169): morphing

--- src/Dataize.hs
+++ src/Dataize.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE LambdaCase #-}
+
 -- SPDX-FileCopyrightText: Copyright (c) 2025 Objectionary.com
 -- SPDX-License-Identifier: MIT
-{-# LANGUAGE LambdaCase #-}
 
 module Dataize where
 
@@ -23,59 +24,74 @@ maybeLambda bds =
         [lambda] -> (Just lambda, rest)
         _ -> (Nothing, bds)
 
--- Returns (X, Y, Z) triple
--- X - maybe labmda binding in deepest formation, if present
--- Y - constructor for expression
--- Z - deepst formation without lambda binding, if present
-atomInHead :: Expression -> (Maybe Binding, Expression -> Expression, Expression)
-atomInHead (ExApplication expr tau) =
-  let (lambda, ctor, formation) = atomInHead expr
-   in (lambda, \exp -> ExApplication (ctor exp) tau, formation)
-atomInHead (ExDispatch expr attr) =
-  let (lambda, ctor, formation) = atomInHead expr
-   in (lambda, \exp -> ExDispatch (ctor exp) attr, formation)
-atomInHead (ExFormation bds) =
-  let (lambda, rest) = maybeLambda bds
-   in (lambda, id, ExFormation rest)
-atomInHead expr = (Nothing, id, ExFormation [])
+formation :: [Binding] -> IO (Maybe Expression)
+formation bds = do
+  let (lambda, bds') = maybeLambda bds
+  case lambda of
+    Just (BiLambda func) -> do
+      obj' <- atom func (ExFormation bds')
+      pure (Just obj')
+    _ -> pure Nothing
 
-morphAtom :: String -> Expression -> (Expression -> Expression) -> IO (Maybe Expression)
-morphAtom func formation ctor
-  | isNF formation = do
-      obj <- atom func formation
-      pure (Just (ctor obj))
-  | otherwise = pure Nothing
+phiDispatch :: String -> Expression -> Maybe Expression
+phiDispatch attr expr = case expr of
+  ExFormation bds -> boundExpr bds
+  _ -> Nothing
+  where
+    boundExpr :: [Binding] -> Maybe Expression
+    boundExpr [] = Nothing
+    boundExpr (bd : bds) = case bd of
+      BiTau (AtLabel attr') expr' -> if attr' == attr then Just expr' else boundExpr bds
+      _ -> boundExpr bds
 
-morph' :: Expression -> IO (Maybe Expression)
-morph' expr = case atomInHead expr of
-  (Just (BiLambda func), ctor, formation) -> morphAtom func formation ctor
-  (Nothing, _, _)
-    | isNF expr -> pure Nothing
-    | otherwise -> do
-        (Program expr') <- rewrite' (Program expr) normalizationRules 25
-        morph expr'
-  (_, _, _) -> pure Nothing
+withTail :: Expression -> Program -> IO (Maybe Expression)
+withTail (ExApplication (ExFormation _) _) _ = pure Nothing
+withTail (ExApplication (ExDispatch ExGlobal _) _) _ = pure Nothing
+withTail (ExApplication expr tau) prog = do
+  Just exp <- withTail expr prog
+  pure (Just (ExApplication exp tau))
+withTail (ExDispatch (ExFormation bds) attr) _ = do
+  Just obj <- formation bds
+  pure (Just (ExDispatch obj attr))
+withTail (ExFormation bds) _ = formation bds
+withTail (ExDispatch (ExDispatch ExGlobal (AtLabel label)) attr) (Program expr) = case phiDispatch label expr of
+  Just obj -> pure (Just (ExDispatch obj attr))
+  _ -> pure Nothing
+withTail (ExDispatch ExGlobal (AtLabel label)) (Program expr) = pure (phiDispatch label expr)
+withTail (ExDispatch expr attr) prog = do
+  Just exp <- withTail expr prog
+  pure (Just (ExDispatch exp attr))
+withTail _ _ = pure Nothing
 
 -- The Morphing function M:<B,S> -> <P,S> maps objects to
 -- primitives, possibly modifying the state of evaluation.
 -- Terminology:
 -- P(e) - is e Primitive, which is either formation without λ binding or termination ⊥
--- N(e1) -> e2 - normalization of e1 leads to e2
+-- N(e) - normalize e
 -- NF(e) - is e in normal form (can't be normalized anymore)
 --
--- Rules:
--- M1: M(e) -> e; if P(e)
--- M2: M([[B1, λ -> F, B2]] * !t) -> M(e2 * !t); where e3 = [[B1,B2]] and NF(e3) and F(e3) -> e2 and !t - possibly empty tail
--- M3: M(e1) -> M(e2); if N(e1) -> e2 and e1 != e2
--- M4: M(e) -> Nothing; in any other cases
-morph :: Expression -> IO (Maybe Expression)
-morph ExTermination = pure (Just ExTermination) -- M1
-morph (ExFormation bds) = case atomInHead (ExFormation bds) of
-  (Just (BiLambda func), ctor, formation) -> morphAtom func formation ctor
-  (_, _, formation) -> pure (Just formation) -- M1
-morph (ExApplication expr tau) = morph' (ExApplication expr tau)
-morph (ExDispatch expr attr) = morph' (ExDispatch expr attr)
-morph _ = pure Nothing
+-- PRIM:   M(e) -> e                              if P(e)
+-- NMZ:    M(e1) -> M(e2)                         if e2 := N(e1) and e1 != e2
+-- LAMBDA: M([B1, λ -> F, B2] * t) -> M(e2 * t)   if e3 := [B1,B2] and e2 := F(e3)
+-- PHI:    M(Q.tau * t) -> M(e * t)               if Q -> [B1, tau -> e, B2], t is tail started with dispatch
+--         M(e) -> nothing                        otherwise
+morph :: Expression -> Program -> IO (Maybe Expression)
+morph ExTermination _ = pure (Just ExTermination) -- PRIM
+morph (ExFormation bds) prog = do
+  resolved <- withTail (ExFormation bds) prog
+  case resolved of
+    Just obj -> morph obj prog -- LAMBDA or PHI
+    _ -> pure (Just (ExFormation bds)) -- PRIM
+morph expr prog = do
+  resolved <- withTail expr prog
+  case resolved of
+    Just obj -> morph obj prog
+    _ ->
+      if isNF expr
+        then pure Nothing
+        else do
+          (Program expr') <- rewrite' (Program expr) normalizationRules 25 -- NMZ
+          morph expr' prog
 
 atom :: String -> Expression -> IO Expression
 atom "L_org_eolang_number_plus" self = pure (ExFormation [])
--- test/DataizeSpec.hs
+++ test/DataizeSpec.hs
@@ -3,25 +3,29 @@
 
 module DataizeSpec (spec) where
 
-import Ast (Binding (BiDelta), Expression (ExFormation, ExTermination, ExThis, ExGlobal, ExDispatch), Attribute (AtLabel))
+import Ast (Attribute (AtLabel), Binding (BiDelta, BiTau), Expression (ExDispatch, ExFormation, ExGlobal, ExTermination, ExThis), Program (Program))
 import Control.Monad
 import Dataize (morph)
 import Test.Hspec
 
-testMorph :: [(String, Expression, Maybe Expression)] -> SpecWith (Arg Expectation)
+testMorph :: [(String, Expression, Expression, Maybe Expression)] -> SpecWith (Arg Expectation)
 testMorph useCases =
-  forM_ useCases $ \(desc, input, output) ->
+  forM_ useCases $ \(desc, input, prog, output) ->
     it desc $ do
-      res <- morph input
+      res <- morph input (Program prog)
       res `shouldBe` output
 
 spec :: Spec
 spec =
   describe "morph" $
     testMorph
-      [ ("[[ D> 00- ]] => [[ D> 00- ]]", ExFormation [BiDelta "00-"], Just (ExFormation [BiDelta "00-"])),
-        ("T => T", ExTermination, Just ExTermination),
-        ("$ => X", ExThis, Nothing),
-        ("Q => X", ExGlobal, Nothing),
-        ("Q.x", ExDispatch ExGlobal (AtLabel "x"), Nothing)
+      [ ("[[ D> 00- ]] => [[ D> 00- ]]", ExFormation [BiDelta "00-"], ExGlobal, Just (ExFormation [BiDelta "00-"])),
+        ("T => T", ExTermination, ExGlobal, Just ExTermination),
+        ("$ => X", ExThis, ExGlobal, Nothing),
+        ("Q => X", ExGlobal, ExGlobal, Nothing),
+        ( "Q.x (Q -> [[ x -> [[]] ]]) => [[]]",
+          ExDispatch ExGlobal (AtLabel "x"),
+          ExFormation [BiTau (AtLabel "x") (ExFormation [])],
+          Just (ExFormation [])
+        )
       ]
