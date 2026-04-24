71bad84b715519c0093e1a3ab6d106462872fd3e
maxonfjvipon <mtrunnikov@gmail.com>
2026-04-24 19:24:25 +0300

    bug(#651): shorter

--- src/Builder.hs
+++ src/Builder.hs
@@ -41,7 +41,7 @@ data BuildException
   deriving (Exception)
 
 metaMsg :: Text -> String
-metaMsg meta = printf "meta '%s' is either does not exist or refers to an inappropriate term" (T.unpack meta)
+metaMsg = printf "meta '%s' is either does not exist or refers to an inappropriate term" . T.unpack
 
 type Built a = Either String a
 
